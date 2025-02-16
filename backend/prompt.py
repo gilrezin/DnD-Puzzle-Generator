0664cc4b7a60d0b535a0a8567c7f898a90b158a9
Gil Rezin <gil.rezin@hotmail.com>
2025-02-15 21:18:22 -0800

    minor edit to prompt

--- backend/prompt.py
+++ backend/prompt.py
@@ -10,10 +10,11 @@ headers = {"Authorization": "Bearer hewWvVr1qsqzHQvVZSxWZaoQ5ZnXI1xrXClknLgk"}
 
 # run function for LLM
 def run(model, inputs):
-    input = {"messages": inputs, "max_tokens": 4096}
+    input = {"messages": inputs, "max_tokens": 6000, "temperature": 0.3}
     response = requests.post(   f"{API_BASE_URL}{model}", headers=headers, json=input)
     return response.json()
 
+# TODO: from our user's input, convert that to our user's content
 
 # removes all plaintext lines of 1 word or less
 def read_filtered_lines(file_path):
@@ -21,9 +22,7 @@ def read_filtered_lines(file_path):
         filtered_text = "\n".join(line.strip() for line in file if len(line.split()) > 1)
     return filtered_text
 
-# TODO: from our user's input, convert that to our user's content
-
-# example code generation ---
+# --- example code generation ---
 summary = """Our players are on a quest from the city of Waterdeep to the far north in rumors of a mighty goddess 
      with the power to grant wishes to anyone brave enought to survive the journey to her throne room. From where we last left off, our players have not traveled
       far from Waterdeep, resting by the coastal town of Silverport where rumors of an oncoming army from a faraway lands in search of the goddess are abound.
@@ -38,15 +37,15 @@ summary += read_filtered_lines(file)
 file = 'Gareth_Stonecobble.txt'
 summary += read_filtered_lines(file)
 
-# end example code ---
+# --- end example code generation ---
 
 # LLM prompt
 inputs = [
     { "role": "system", "content": """You are a co-dungeon master for a game of Dungeons & Dragons. Given a story and characters provided by the user, 
-        you will generate the following, each as their own paragraph & header:
+        you will generate the following, each paragraph surrounded by double square brackets:
         \n- A generalized storyline specifying what challenges await the players within the next game session. Be vague in the path the players take, as that may change over the course of the session.
         \n- A list of friendly NPCs the players may encounter. List their traits, their items, and their personalities.
-        \n- A list of significant enemies the players may encounter. List their traits, items, stats, and movesets.
+        \n- A list of significant enemies the players may encounter. List their traits, motivations, items, stats, and movesets.
         \n- Puzzles/Challenges that the players may encounter. Do not provide the solutions to these problems, but rather potential solutions - incorporate spells, items, and character traits that players have into the problems and their solutions.""" },
     
     { "role": "user", "content": summary}
